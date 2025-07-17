02d322ad70f7309d51fc13600c67a9c0a9a44644
maziazi <mazis9651@gmail.com>
2025-07-17 11:41:38 +0700

    Initial Commit

---
+++ testPlayTests/testPlayTests.swift
@@ -0,0 +1,17 @@
+//
+//  testPlayTests.swift
+//  testPlayTests
+//
+//  Created by Muhamad Azis on 17/07/25.
+//
+
+import Testing
+@testable import testPlay
+
+struct testPlayTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
